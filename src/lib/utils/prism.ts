c5f251d7a3db2da254387bbe75942b1355b3b7b0
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-24 00:54:36 +0800

    feat: prism helper

---
+++ src/lib/utils/prism.ts
@@ -0,0 +1,26 @@
+// Prism is a jetstream/jetstream-compatible fork for receiving filtered events from the firehose
+import { PRISM_URL } from "@/lib/env";
+import WebSocket from "ws";
+
+export const connectToPrism = ({
+    wantedCollections,
+    wantedDids,
+    cursor,
+}: {
+    wantedCollections?: Array<string>;
+    wantedDids?: Array<string>;
+    cursor: number;
+}) => {
+    const endpoint = PRISM_URL;
+    if (wantedCollections)
+        wantedCollections.forEach((collection) => {
+            endpoint.searchParams.append("wantedCollections", collection);
+        });
+    if (wantedDids)
+        wantedDids.forEach((did) => {
+            endpoint.searchParams.append("wantedDids", did);
+        });
+    if (cursor) endpoint.searchParams.append("cursor", cursor.toString());
+
+    return new WebSocket(endpoint);
+};
