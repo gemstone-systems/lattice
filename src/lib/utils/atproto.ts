da18e1a4db0e119631fda5ce52a5f72233435733
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-24 15:22:52 +0800

    feat: string to at uri

--- src/lib/utils/atproto.ts
+++ src/lib/utils/atproto.ts
@@ -4,7 +4,11 @@ import type {
     Did,
     DidDocument,
 } from "@/lib/types/atproto";
-import { atprotoHandleSchema } from "@/lib/types/atproto";
+import {
+    atprotoHandleSchema,
+    atUriAuthoritySchema,
+    nsidSchema,
+} from "@/lib/types/atproto";
 import { comAtprotoRepoGetRecordResponseSchema } from "@/lib/types/lexicon/com.atproto.repo.getRecord";
 import type { Result } from "@/lib/utils/result";
 import type { DidDocumentResolver } from "@atcute/identity-resolver";
@@ -16,6 +20,7 @@ import {
     WebDidDocumentResolver,
     WellKnownHandleResolver,
 } from "@atcute/identity-resolver";
+import { z } from "zod";
 
 export const getRecordFromAtUri = async ({
     authority,
@@ -123,4 +128,59 @@ export const atUriToString = ({ authority, collection, rKey }: AtUri) => {
     result += collection ? `/${collection}` : "";
     result += rKey ? `/${rKey}` : "";
     return result;
+};
+
+export const stringToAtUri = (str: string): Result<AtUri, unknown> => {
+    const isValidAtUri = atUriRegexp.test(str);
+    if (!isValidAtUri)
+        return {
+            ok: false,
+            error: { message: "Input string was not a valid at:// URI" },
+        };
+
+    const fragments = str.split("/");
+    if (fragments.length <= 2)
+        return {
+            ok: false,
+            error: { message: "Input string was not a valid at:// URI." },
+        };
+
+    const {
+        success: authorityParseSuccess,
+        error: authorityParseError,
+        data: authorityParsed,
+    } = atUriAuthoritySchema.safeParse(fragments[1]);
+    if (!authorityParseSuccess)
+        return {
+            ok: false,
+            error: {
+                message:
+                    "Input at:// URI was a valid shape, but somehow could not parse the first fragment as a valid authority.",
+                details: z.treeifyError(authorityParseError),
+            },
+        };
+
+    const {
+        success: nsidParseSuccess,
+        error: nsidParseError,
+        data: nsidParsed,
+    } = nsidSchema.safeParse(fragments[2]);
+    if (fragments[2] && !nsidParseSuccess)
+        return {
+            ok: false,
+            error: {
+                message:
+                    "Input at:// URI was a valid shape and had a second fragment, but was somehow not a valid NSID.",
+                details: z.treeifyError(nsidParseError),
+            },
+        };
+
+    return {
+        ok: true,
+        data: {
+            authority: authorityParsed,
+            collection: nsidParsed,
+            rKey: fragments[3],
+        },
+    };
 };
