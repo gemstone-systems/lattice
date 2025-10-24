9d5f1c15e11de8670b858ab8dcde9919ac41b5e9
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-24 15:49:05 +0800

    feat: string is domain helper

---
+++ src/lib/utils/domains.ts
@@ -0,0 +1,12 @@
+/**
+ * Checks if a string is a domain
+ * does NOT do TLD-level checks. Any domain/NSID-like string will pass this check
+ */
+export const isDomain = (str: string) => {
+    try {
+        const url = new URL(str.includes("://") ? str : `https://${str}`);
+        return url.hostname.includes(".") && !url.hostname.startsWith(".");
+    } catch {
+        return false;
+    }
+};
